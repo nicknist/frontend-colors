4137ae3d0a26916dec3f1549e55220c21cc55909
Nick Nist <nicknistoc@gmail.com>
2020-02-07 11:15:21 -0700

    Add enzyme and first snapshot test

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  })
+})
