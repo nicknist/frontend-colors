60dd7a1b8078f96e2ec9a280253c0576bac8af14
Nick Nist <nicknistoc@gmail.com>
2020-02-09 12:23:43 -0700

    Add lock and unlock functionality for color randomizer

--- src/Components/ColorContainer/ColorContainer.js
+++ src/Components/ColorContainer/ColorContainer.js
@@ -2,28 +2,39 @@ import React, { Component } from 'react';
 import ColorPicker from '../ColorPicker/ColorPicker';
 import PaletteContainer from '../PaletteContainer/PaletteContainer';
 
-const getFiveRandomColors = () => {
+const getFiveRandomColors = (colors) => {
   const randomColor = () => `#${Math.floor(Math.random()*16777215).toString(16)}`;
-  return [randomColor(), randomColor(), randomColor(), randomColor(), randomColor()]
+  if (colors.length === 0) {
+    return [
+      { color: randomColor(), locked: false },
+      { color: randomColor(), locked: false },
+      { color: randomColor(), locked: false },
+      { color: randomColor(), locked: false },
+      { color: randomColor(), locked: false }]
+  }
+  let newArray = [];
+  colors.forEach(color => {
+    if (color.locked) {
+      newArray.push({ color: color.color, locked: true });
+    } else {
+      newArray.push({ color: randomColor(), locked: false });
+    }
+  })
+  return newArray;
 }
-
-//[ {
-// color: ,
-// locked:
-// }]
 
 class ColorContainer extends Component {
   constructor() {
     super();
     this.state = {
-      colors: getFiveRandomColors(),
+      colors: getFiveRandomColors([]),
       palettes: [],
       paletteName: ''
     }
   }
 
   handleClick = () => {
-    this.setState({ colors: getFiveRandomColors() })
+    this.setState({ colors: getFiveRandomColors(this.state.colors) })
   }
 
   handleChange = (color, id) => {
@@ -36,16 +47,21 @@ class ColorContainer extends Component {
     this.setState({ paletteName: value })
   }
 
+  lockColor = (colorHex, id, locked) => {
+    let newColors = [...this.state.colors];
+    let lockedColor = { color: colorHex, locked: !locked };
+    newColors.splice(id, 1, lockedColor);
+    this.setState({ colors: newColors })
+  }
+
   addPalette = () => {
     this.setState({ palettes: [...this.state.palettes, { colors: this.state.colors, name: this.state.paletteName }] })
   }
 
   deletePalette = async (name) => {
     let palette = this.state.palettes.findIndex(palette => name === palette.name)
-    console.log(palette);
     let array = [...this.state.palettes];
-    console.log(array);
-    array.splice(1, palette);
+    array.splice(palette, 1);
     await this.setState({ palettes: array })
   }
 
@@ -54,11 +70,11 @@ class ColorContainer extends Component {
     return (
       <>
         <section className='container'>
-        <ColorPicker color={colors[0]} handleChange={this.handleChange} number={0}/>
-        <ColorPicker color={colors[1]} handleChange={this.handleChange} number={1}/>
-        <ColorPicker color={colors[2]} handleChange={this.handleChange} number={2}/>
-        <ColorPicker color={colors[3]} handleChange={this.handleChange} number={3}/>
-        <ColorPicker color={colors[4]} handleChange={this.handleChange} number={4}/>
+        <ColorPicker color={colors[0].color} locked={colors[0].locked} handleChange={this.handleChange} lock={this.lockColor} number={0}/>
+        <ColorPicker color={colors[1].color} locked={colors[1].locked} handleChange={this.handleChange} lock={this.lockColor} number={1}/>
+        <ColorPicker color={colors[2].color} locked={colors[2].locked} handleChange={this.handleChange} lock={this.lockColor} number={2}/>
+        <ColorPicker color={colors[3].color} locked={colors[3].locked} handleChange={this.handleChange} lock={this.lockColor} number={3}/>
+        <ColorPicker color={colors[4].color} locked={colors[4].locked} handleChange={this.handleChange} lock={this.lockColor} number={4}/>
         </section>
         <button className='randomizer' onClick={this.handleClick}>RANDOMIZE COLORS</button>
         <input type='text' className='palette-name' onChange={(e) => this.handleUpdate(e.target.value)}/>
