97f25af0a45cdf4b6dd7a9095e1c17aa722f1f8b
Nick Nist <nicknistoc@gmail.com>
2020-02-09 12:53:32 -0700

    Add ProjectContainer to App Component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,12 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 import ColorContainer from '../ColorContainer/ColorContainer';
 import Header from '../Header/Header';
+import ProjectContainer from '../ProjectContainer/ProjectContainer';
 
+
+//currently hard coded in one project for the time being
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      projects: []
+      projects: [
+        { name: 'PROJECT 1',
+          palettes: [
+          { name: 'NUMBAH1',
+            colors: [
+              { color: 'white', locked: false },
+              { color: 'blue', locked: false },
+              { color: 'red', locked: false },
+              { color: 'orange', locked: false },
+              { color: 'rebeccapurple', locked: false }
+            ]
+          }
+        ]
+        }
+      ]
     }
   }
 
@@ -16,6 +33,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <ColorContainer />
+        <ProjectContainer projects={this.state.projects}/>
       </div>
     );
   }
