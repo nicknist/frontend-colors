b351112687ad7c5906a4aa8eb98cd2204020911f
Nick Nist <nicknistoc@gmail.com>
2020-02-09 13:24:07 -0700

    Update Project component to have a deleteFromProject button on each Palette

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Palette from '../Palette/Palette';
 
 //will need some sort of deleteProject thing going down here
+//Also need the deletePalette to take it off the project...
 
 const Project = ({ info }) => {
   return (
     <>
       <h1>{info.name}</h1>
       <p>Delete Project Button incoming</p>
-      {info.palettes.map(palette => <Palette key={palette.colors[0].color} name={palette.name} colors={palette.colors} />)}
+      {info.palettes.map(palette => <Palette key={palette.colors[0].color} name={palette.name} colors={palette.colors} deletePaletteFromProject={() => console.log('NUKE THE PALETTE')}/>)}
     </>
   )
 }
