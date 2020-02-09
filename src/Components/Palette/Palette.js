3889a03ead3b33ac4c808713107b9b3a6d575ef2
Nick Nist <nicknistoc@gmail.com>
2020-02-09 13:23:30 -0700

    Update Palette Component to have a deleteFromProject function

--- src/Components/Palette/Palette.js
+++ src/Components/Palette/Palette.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Palette = ({ name, colors, deletePalette }) => {
+const Palette = ({ name, colors, deletePalette, deletePaletteFromProject }) => {
   const colorDivs = colors.map(color => {
     const background = {
       margin: '10px',
@@ -20,6 +20,7 @@ const Palette = ({ name, colors, deletePalette }) => {
     <>
       <h1 className='white'>{name}</h1>
       { deletePalette ? <button id={name} onClick={() => deletePalette(name)} className='randomizer'>Delete This Palette</button> : ''}
+      { deletePaletteFromProject ? <button id={name} onClick={() => deletePaletteFromProject(name)} className='randomizer'>Delete This Palette From Project</button> : ''}
       <article className='palette-div'>
         {colorDivs}
       </article>
