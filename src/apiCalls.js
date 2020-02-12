507d83b923b564f36c552ddd4f27354aff0e2d48
Nick Nist <nicknistoc@gmail.com>
2020-02-12 07:51:52 -0700

    Add apiCalls and testing for post and delete

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -12,7 +12,7 @@ export const getProject = (id) => {
   return fetch(`http://backend-colors.herokuapp.com/api/v1/projects/${id}`)
     .then(response => {
       if(!response.ok) {
-        throw Error('Can not find Palette');
+        throw Error('Can not find Project');
       }
       return response.json();
     })
@@ -22,7 +22,7 @@ export const getPalettes = () => {
   return fetch('http://backend-colors.herokuapp.com/api/v1/palettes')
     .then(response => {
       if (!response.ok) {
-        throw Error('Can not find any projects!')
+        throw Error('Can not find any palettes!')
       }
       return response.json();
     })
@@ -35,5 +35,55 @@ export const getPalette = (id) => {
         throw Error('Can not find Palette');
       }
       return response.json();
+    })
+}
+
+export const postPalette = (palette) => {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(palette),
+    headers: { 'Content-Type': 'application/json' }
+  };
+  return fetch('http://backend-colors.herokuapp.com/api/v1/palettes', options)
+    .then(response => {
+      if (!response.ok) {
+        throw Error('Could not post your Palette')
+      }
+      return response.json();
+    })
+};
+
+export const postProject = (project) => {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(project),
+    headers: { 'Content-Type': 'application/json' }
+  };
+  return fetch('http://backend-colors.herokuapp.com/api/v1/projects', options)
+    .then(response => {
+      if (!response.ok) {
+        throw Error('Can not post Project')
+      }
+      return response.json();
+    })
+}
+
+export const deletePalette = (id) => {
+  return fetch(`http://backend-colors.herokuapp.com/api/v1/palettes/${id}`, { method: 'DELETE' })
+    .then(response => {
+      if (!response.ok) {
+        throw Error('Could not delete that palette')
+      }
+      return 'Nice Delete';
+    })
+}
+
+export const deleteProject = (id) => {
+  return fetch(`http://backend-colors.herokuapp.com/api/v1/projects/${id}`, { method: 'DELETE' })
+    .then(response => {
+      if (!response.ok) {
+        throw Error('Could not delete that project')
+      }
+      return 'Oh Hello';
     })
 }
--- src/apiCalls.test.js
+++ src/apiCalls.test.js
@@ -1,7 +1,341 @@
-import { getProjects, getProject, getPalettes, getPalette } from './apiCalls';
+import { getProjects, getProject, getPalettes, getPalette, postPalette, postProject, deletePalette, deleteProject } from './apiCalls';
 
 describe('apiCalls', () => {
-  it('should run THIS TEST IS NOT REAL', () => {
-    console.log('WE DID IT');
-  })
-})
+  describe('getProjects', () => {
+    let mockProjects;
+
+    beforeEach(() => {
+      mockProjects = [
+        {
+          title: 'Hot pants',
+          project1_name: 'Colins house',
+          project2_name: 'Nicks house',
+          project3_name: 'Colin and Nicks Crayons'
+        }
+      ];
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockProjects)
+        })
+      });
+    });
+
+    it('should be called with the correct url', () => {
+      getProjects();
+      expect(window.fetch).toHaveBeenCalledWith('http://backend-colors.herokuapp.com/api/v1/projects');
+    });
+
+    it('should get back projects when getProjects has a happy path', () => {
+      expect(getProjects()).resolves.toEqual(mockProjects);
+    });
+
+    it('should send back an error when the response is not ok', () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        })
+      });
+      expect(getProjects()).rejects.toEqual(Error('Can not find any projects!'))
+    });
+  });
+
+  describe('getProject', () => {
+    let mockProject;
+
+    beforeEach(() => {
+      mockProject = {
+        title: 'Hot pants',
+        project1_name: 'Colins house',
+        project2_name: 'Nicks house',
+        project3_name: 'Colin and Nicks Crayons'
+      };
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockProject)
+        })
+      });
+    });
+
+    it('should be called with the correct url', () => {
+      getProject(1);
+      expect(window.fetch).toHaveBeenCalledWith(`http://backend-colors.herokuapp.com/api/v1/projects/1`)
+    });
+
+    it('should return a project when called', () => {
+      expect(getProject(1)).resolves.toEqual(mockProject)
+    });
+
+    it('should throw an error when the response is not ok', () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        });
+      });
+      expect(getProject(1)).rejects.toEqual(Error('Can not find Project'))
+    });
+  });
+
+  describe('getPalettes', () => {
+    let mockPalettes;
+
+    beforeEach(() => {
+      mockPalettes = [
+        {
+          title: 'TEST NUKE PALETTE',
+          color1: '#867CBC',
+          color2: '#E2F7EF',
+          color3: '#23889A',
+          color4: '#A6E508',
+          color5: '#D15120'
+        },
+        {
+          title: 'Colins house',
+          color1: '#E2A7EF',
+          color2: '#867CCC',
+          color3: '#23888A',
+          color4: '#D15120',
+          color5: '#A6E308'
+        },
+        {
+          title: 'Nicks house',
+          color1: '#867CCC',
+          color2: '#E2A7EF',
+          color3: '#23888A',
+          color4: '#A6E308',
+          color5: '#D15120'
+        },
+        {
+          title: 'Colin and Nicks Crayons',
+          color1: '#867CBC',
+          color2: '#E2F7EF',
+          color3: '#23889A',
+          color4: '#A6E508',
+          color5: '#D15120'
+        },
+      ];
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockPalettes)
+        });
+      });
+    });
+
+    it('should return an array of Palettes when called', () => {
+      expect(getPalettes()).resolves.toEqual(mockPalettes);
+    });
+
+    it('should be called with the correct url', () => {
+      getPalettes();
+      expect(window.fetch).toHaveBeenCalledWith('http://backend-colors.herokuapp.com/api/v1/palettes');
+    });
+
+    it('should throw an error when response is not ok', () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        });
+      });
+      expect(getPalettes()).rejects.toEqual(Error('Can not find any palettes!'))
+    });
+  });
+
+  describe('getPalette', () => {
+    let mockPalette;
+
+    beforeEach(() => {
+      mockPalette = {
+        title: 'TEST NUKE PALETTE',
+        color1: '#867CBC',
+        color2: '#E2F7EF',
+        color3: '#23889A',
+        color4: '#A6E508',
+        color5: '#D15120'
+      };
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockPalette)
+        });
+      });
+    });
+
+    it('should return a palette when called', () => {
+      expect(getPalette(2)).resolves.toEqual(mockPalette);
+    });
+
+    it('should be called with the correct url', () => {
+      getPalette(3);
+      expect(window.fetch).toHaveBeenCalledWith(`http://backend-colors.herokuapp.com/api/v1/palettes/3`)
+    });
+
+    it('should throw an error when the response is not ok', () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        });
+      });
+      expect(getPalette(1)).rejects.toEqual(Error('Can not find Palette'))
+    })
+  });
+
+  describe('postPalette', () => {
+    let mockPalette;
+    let mockOptions;
+
+    beforeEach(() => {
+      mockPalette = {
+        title: 'TEST POST PALETTE',
+        color1: '#867CBC',
+        color2: '#E2F7EF',
+        color3: '#23889A',
+        color4: '#A6E508',
+        color5: '#D15120'
+      };
+      mockOptions = {
+        method: 'POST',
+        body: JSON.stringify(mockPalette),
+        headers: { 'Content-Type': 'application/json' }
+      }
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockPalette)
+        });
+      });
+    });
+
+    it('should return a mockPalette when posted', () => {
+      expect(postPalette(mockPalette)).resolves.toEqual(mockPalette);
+    });
+
+    it('should be called with the correct url and options', () => {
+      postPalette(mockPalette)
+      expect(window.fetch).toHaveBeenCalledWith('http://backend-colors.herokuapp.com/api/v1/palettes', mockOptions)
+    });
+
+    it('should throw an error when response is not ok', () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        });
+      });
+      expect(postPalette(mockPalette)).rejects.toEqual(Error('Could not post your Palette'));
+    });
+  });
+
+  describe('postProject', () => {
+    let mockProject;
+    let mockOptions;
+
+    beforeEach(() => {
+      mockProject = [
+        {
+          title: 'Hot pants',
+          project1_name: 'Colins house',
+          project2_name: 'Nicks house',
+          project3_name: 'Colin and Nicks Crayons'
+        }
+      ];
+      mockOptions = {
+        method: 'POST',
+        body: JSON.stringify(mockProject),
+        headers: { 'Content-Type': 'application/json' }
+      };
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockProject)
+        });
+      });
+    });
+
+    it('should return a project when called', () => {
+      expect(postProject(mockProject)).resolves.toEqual(mockProject);
+    });
+
+    it('should be called with the correct url and options', () => {
+      postProject(mockProject);
+      expect(window.fetch).toHaveBeenCalledWith('http://backend-colors.herokuapp.com/api/v1/projects', mockOptions);
+    });
+
+    it('should throw an error when response is not ok', () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        });
+      });
+      expect(postProject(mockProject)).rejects.toEqual(Error('Can not post Project'));
+    });
+  });
+
+  describe('deletePalette', () => {
+    let mockOptions;
+
+    beforeEach(() => {
+      mockOptions = {
+        method: 'DELETE'
+      };
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve('Oh Hello')
+        });
+      });
+    });
+
+    it('should return a hard-coded message when called', () => {
+      expect(deletePalette(2)).resolves.toEqual('Oh Hello');
+    });
+
+    it('should be called with the correct url and options', () => {
+      deletePalette(2);
+      expect(window.fetch).toHaveBeenCalledWith('http://backend-colors.herokuapp.com/api/v1/palettes/2', mockOptions)
+    });
+
+    it('should throw an error when response is not ok', () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        });
+      });
+      expect(deletePalette(3)).rejects.toEqual(Error('Could not delete that palette'));
+    });
+  });
+
+  describe('deleteProject', () => {
+    let mockOptions;
+
+    beforeEach(() => {
+      mockOptions = {
+        method: 'DELETE'
+      };
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve('Oh Waddup')
+        });
+      });
+    });
+
+    it('should return a hard-coded message when called', () => {
+      expect(deleteProject(4)).resolves.toEqual('Oh Waddup');
+    });
+
+    it('should called with correct url and options', () => {
+      deleteProject(2);
+      expect(window.fetch).toHaveBeenCalledWith(`http://backend-colors.herokuapp.com/api/v1/projects/2`, { method: 'DELETE' })
+    })
+
+    it('should throw an error when response is not ok', () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        });
+      });
+      expect(deleteProject(1)).rejects.toEqual(Error('Could not delete that project'));
+    });
+  });
+});
